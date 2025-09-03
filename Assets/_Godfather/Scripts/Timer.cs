0c31c97725aecd6d346695d1e6f2eb72debffa6f
Shahine Benthanane <shahine93300@gmail.com>
2025-09-03 17:11:00 +0200

    TMP + Timer + Game Over Panel In Progress

---
+++ Assets/_Godfather/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _timerText;
+    private float _timeElapsed;
+    
+
+    private void Awake()
+    {
+        _timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeElapsed += Time.deltaTime;
+        // Affiche le temps au format MM:SS:MS
+        _timerText.text = GetTimeString();
+    }
+    
+    public string GetTimeString()
+    {
+        int minutes = Mathf.FloorToInt(_timeElapsed / 60f);
+        int seconds = Mathf.FloorToInt(_timeElapsed % 60f);
+        int milliseconds = Mathf.FloorToInt((_timeElapsed * 100f) % 100f);
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+}
